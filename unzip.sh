43c0ac41a0d3fd59dc53670e968ce11ba10b488b
satyajit nayak <satyajitn15@gmail.com>
2024-04-28 21:32:51 +0530

    update shell script to format xml & rels files recursively.

--- unzip.sh
+++ unzip.sh
@@ -6,4 +6,10 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
+# Ensure xmllint is installed
+command -v xmllint >/dev/null 2>&1 || { echo >&2 "xmllint is required but not installed. Aborting."; exit 1; }
+
+
 unzip "./input/$1" -d "extracted/$1"
+find "extracted/$1" -type f -name "*.xml" -exec xmllint --format {} --output {} \;
+find "extracted/$1" -type f -name "*.rels" -exec xmllint --format {} --output {} \;
